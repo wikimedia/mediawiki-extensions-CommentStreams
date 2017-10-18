1eabe6b4809c78be0b6d5036a280babb16d7c4f8
Cindy Cicalese <cindom@gmail.com>
2017-10-18 16:06:50 +0000

    Removed foreign key that was causing errors on page deletion.
    T178502
    T178503
    
    Change-Id: I7a3336c059f510e5b6fc583f3541170eccedc76a

---
+++ sql/dropForeignKey2.sql
@@ -0,0 +1,2 @@
+ALTER TABLE /*_*/cs_comment_data DROP FOREIGN KEY cs_comment_data_ibfk_1;
+ALTER TABLE /*_*/cs_comment_data DROP INDEX cst_assoc_page_id;
--- sql/votes.sql
+++ sql/votes.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS /*_*/cs_votes
 (
-page_id int(10) unsigned NOT NULL,
-user_id int(10) unsigned NOT NULL,
-vote tinyint NOT NULL,
-INDEX (page_id, user_id)
+cst_v_page_id int(10) unsigned NOT NULL,
+cst_v_user_id int(10) unsigned NOT NULL,
+cst_v_vote tinyint NOT NULL,
+INDEX (cst_v_page_id, cst_v_user_id)
 );
