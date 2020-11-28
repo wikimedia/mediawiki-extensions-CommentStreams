3ee9bb7135654b4c15d83d00ddbf72339467e8dd
Paladox <thomasmulhall410@yahoo.com>
2020-11-28 15:17:07 +0000

    Add missing schema change to commentData
    Change-Id: I3dfd48333b29fe5d938976f5f8e202f013fc0ada

--- sql/commentData.sql
+++ sql/commentData.sql
@@ -4,5 +4,6 @@ cst_page_id int(10) unsigned,
 cst_assoc_page_id int(10) unsigned,
 cst_parent_page_id int(10) unsigned,
 cst_comment_title varbinary(255),
+cst_id varchar(50) DEFAULT "cs-comments",
 PRIMARY KEY (cst_page_id)
 );
