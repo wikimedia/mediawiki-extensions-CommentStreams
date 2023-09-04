f3b82e354e99d04a8b45b384d741410947d041b2
Mark A. Hershberger <mah@nichework.com>
2023-09-03 21:00:00 -0400

    Simple phan update
    * Add error-format-test target to composer that will produce
      output that corresponds to the format used by a vim Quickfix
      window, a VSCode ProblemMatcher, PHPStorm's output filter, or an
      Emacs compile buffer.
    * DRY out .phan/config.php a little by moving extension list to a
      variable and open it up so that it is easy to add more later.
    
    Change-Id: I1f2db634b5cce5b6998e82a663fc910a6c776d6b

--- .phan/config.php
+++ .phan/config.php
@@ -1,19 +1,11 @@
 <?php
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+$extensions = [ "Echo", "SocialProfile" ];
+$dirList = array_map( fn( $dir ): string => "../../extensions/$dir", $extensions );
 
-$cfg['directory_list'] = array_merge(
-	$cfg['directory_list'],
-	[
-		'../../extensions/Echo',
-		'../../extensions/SocialProfile',
-	]
-);
+$cfg['directory_list'] = array_merge( $cfg['directory_list'] ?? [], $dirList );
 $cfg['exclude_analysis_directory_list'] = array_merge(
-	$cfg['exclude_analysis_directory_list'],
-	[
-		'../../extensions/Echo',
-		'../../extensions/SocialProfile',
-	]
+	$cfg['exclude_analysis_directory_list'] ?? [], $dirList
 );
 
 return $cfg;
