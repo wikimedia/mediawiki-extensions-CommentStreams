369c792e0f330dcf75c653d34fcd45561f652db0
Mark A. Hershberger <mah@nichework.com>
2023-11-11 22:59:06 -0500

    Update gensql.sh
    * Apply shellcheck fixes
    * Use MW_INSTALL_PATH if available
    * Also allow script to be run from anywhere
    
    Change-Id: I30cd42325273df85bcf1a7bf6663fb87252586c6
    (cherry picked from commit 500dfc9a9b559438d5ff0a12c18eff51527d006f)

--- sql/gensql.sh
+++ sql/gensql.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-dir=`dirname "$0"`
-echo $dir
+dir=$(dirname "$0")
+MW_INSTALL_PATH=${MW_INSTALL_PATH:-$dir/../../..}
 for db in mysql postgres sqlite
 do
 	for schema in cs_comments cs_replies cs_votes cs_watchlist cs_associated_pages
 	do
 		echo $db : $schema
 
-		php $dir/../../../maintenance/generateSchemaSql.php --json $schema.json --sql $db/$schema.sql --type=$db
+		php "${MW_INSTALL_PATH}/maintenance/generateSchemaSql.php" --json $dir/$schema.json --sql $dir/$db/$schema.sql --type=$db
 	done
 	for patch in cs_votes_comment_id_patch cs_watchlist_comment_id_patch
   	do
