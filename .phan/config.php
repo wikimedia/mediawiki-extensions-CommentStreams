933247ee8a7417b6398db2d5e33dbbe85c7b0a0c
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2024-04-27 10:39:16 +0000

    build: Updating dependencies
    composer:
    * mediawiki/mediawiki-codesniffer: 41.0.0 → 43.0.0
    * php-parallel-lint/php-parallel-lint: 1.3.2 → 1.4.0
    
    npm:
    * eslint-config-wikimedia: 0.25.0 → 0.27.0
    * grunt-banana-checker: 0.11.0 → 0.11.1
    * grunt-eslint: 24.0.1 → 24.3.0
    * grunt-stylelint: 0.18.0 → 0.19.0
    * stylelint-config-wikimedia: 0.15.0 → 0.16.1
    
    Change-Id: I25c2d5c988b6222fea58123676dad2cedd6a5bd3

--- .phan/config.php
+++ .phan/config.php
@@ -1,7 +1,7 @@
 <?php
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 $extensions = [ "Echo", "SocialProfile" ];
-$dirList = array_map( fn( $dir ): string => "../../extensions/$dir", $extensions );
+$dirList = array_map( fn ( $dir ): string => "../../extensions/$dir", $extensions );
 
 $cfg['directory_list'] = array_merge( $cfg['directory_list'] ?? [], $dirList );
 $cfg['exclude_analysis_directory_list'] = array_merge(
