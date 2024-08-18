c380ae9ac614d5f0328ae0264c24bb159ff20634
Umherirrender <umherirrender_de.wp@web.de>
2024-08-18 21:36:02 +0200

    Remove indirect calls to IDBAccessObject::READ_* constants
    Title/WikiPage::READ_NORMAL are removed with 1.42 (c04f1d64d6),
    IDBAccessObject exists since 1.20
    
    Change-Id: I5d0888977daf3e7381b756e96814c79f18ed1a1a
    (cherry picked from commit 443885f8371edd27ba5a8996b7d3955568e93690)

--- includes/SMWInterface.php
+++ includes/SMWInterface.php
@@ -23,6 +23,7 @@ namespace MediaWiki\Extension\CommentStreams;
 
 use ConfigException;
 use ExtensionRegistry;
+use IDBAccessObject;
 use JobQueueGroup;
 use MediaWiki\Config\ServiceOptions;
 use MediaWiki\Page\WikiPageFactory;
@@ -175,7 +176,7 @@ class SMWInterface {
 			return true;
 		}
 
-		$pageId = $subject->getTitle()->getArticleID( Title::READ_LATEST );
+		$pageId = $subject->getTitle()->getArticleID( IDBAccessObject::READ_LATEST );
 
 		$comment = $this->commentStreamsStore->getComment( $pageId );
 		if ( !$comment ) {
