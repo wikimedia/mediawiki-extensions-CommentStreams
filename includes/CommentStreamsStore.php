0cfc9dc98893f5b7819caf91af2916a261e85eff
d.savuljesku <d.savuljesku@gmail.com>
2025-01-13 12:11:37 +0100

    Fix fatal when selecting from a subquery
    Bug: T373511
    Change-Id: Iec6bd83222e3f6f788a46935554c822b63075b0e

--- includes/CommentStreamsStore.php
+++ includes/CommentStreamsStore.php
@@ -34,6 +34,7 @@ use User;
 use Wikimedia\Rdbms\IDatabase;
 use Wikimedia\Rdbms\ILoadBalancer;
 use Wikimedia\Rdbms\IResultWrapper;
+use Wikimedia\Rdbms\Subquery;
 use WikiPage;
 use WikitextContent;
 
@@ -234,7 +235,7 @@ class CommentStreamsStore {
 			->fields( [
 				'page_id'
 			] )
-			->from( '(' . $union->getSQL() . ') AS union_table' )
+			->from( new Subquery( $union->getSQL() ), 'union_table' )
 			->leftJoin( 'page', 'page', [
 				'union_page_id=page_id'
 			] )
