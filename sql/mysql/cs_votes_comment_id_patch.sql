67c7d97b4c8bf25d1152b6023efe6db953a8d53a
Ian Wojtowicz <ian@graphitedesignlabs.com>
2025-08-13 22:57:59 -0700

    Update cs_votes_comment_id_patch.sql

--- sql/mysql/cs_votes_comment_id_patch.sql
+++ sql/mysql/cs_votes_comment_id_patch.sql
@@ -2,7 +2,7 @@
 -- Source: cs_votes_comment_id_patch.json
 -- Do not modify this file directly.
 -- See https://www.mediawiki.org/wiki/Manual:Schema_changes
-DROP INDEX cst_v_index ON /*_*/cs_votes;
+DROP INDEX IF EXISTS cst_v_index ON /*_*/cs_votes;
 ALTER TABLE /*_*/cs_votes
   CHANGE cst_v_page_id cst_v_comment_id INT UNSIGNED NOT NULL;
 
